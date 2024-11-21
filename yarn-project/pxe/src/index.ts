b97b0e35e4b348f8c0ad2680c688254245b55db9
sklppy88 <esau@aztecprotocol.com>
2024-11-21 21:03:02 +0000

    init

--- yarn-project/pxe/src/index.ts
+++ yarn-project/pxe/src/index.ts
@@ -12,3 +12,4 @@ export * from '@aztec/key-store';
 export * from './database/index.js';
 export { ContractDataOracle } from './contract_data_oracle/index.js';
 export { PrivateFunctionsTree } from './contract_data_oracle/private_functions_tree.js';
+export { SimulatorOracle } from './simulator_oracle/index.js';
